e12207f947284291c2d7c75e4601a01bfc9601bf
abdulrahimnoh-dev <abdulrahim.noh@pendidikanjais.my>
2026-08-31 14:56:52 +0800

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'kafa-app-master-v1';
+
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
